71b52ad66824566e736401706961dfe9c7de35c0
YusufCanBektas <yusufcan.bektas@hotmail.com>
2026-06-15 08:50:04 +0200

    Update CameraController.cs
    CameraController.cs wurde dem Spiel angepasst und in Unity wurde die Positionen ebenfalls angepasst.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
-namespace schmup
-{
+namespace schmup{
    
    public class CameraController : MonoBehaviour
    {
+       [SerializeField] Transform player; 
+       [SerializeField] float speed = 2f;
+
+       void Start(){ 
+           transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+       }
        
+       void Update() {
+           // Bewegung der Kamera entlang des Hintergrundes in der konstanten schnelligkeit
+           transform.position += Vector3.right * speed * Time.deltaTime;
+           
+       }
    }
    
 }
