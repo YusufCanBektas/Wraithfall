d8aa7d7274184c18e56e07743b9b17917f66b4ee
YusufCanBektas <yusufcan.bektas@hotmail.com>
2026-07-01 11:33:56 +0200

    Update InputReader.cs

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -1,22 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace schmup
-{
+namespace schmup {
     [RequireComponent(typeof(PlayerInput))]
     public class InputReader : MonoBehaviour
     {
         PlayerInput playerInput;
         InputAction moveAction;
         InputAction boostAction;
+        InputAction shootAction;
 
         public Vector2 Move => moveAction.ReadValue<Vector2>();
         public bool Boost => boostAction.IsPressed();
+        public bool Shoot => shootAction.IsPressed();
 
         void Start() {
             playerInput = GetComponent<PlayerInput>();
             moveAction = playerInput.actions["Move"];
             boostAction = playerInput.actions["Boost"];
+            shootAction = playerInput.actions["shoot"];
         }
     }
 }
