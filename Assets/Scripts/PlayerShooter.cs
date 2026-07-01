d78db437574b35e055ac496195c2c721493094b6
YusufCanBektas <yusufcan.bektas@hotmail.com>
2026-07-01 11:33:53 +0200

    Create PlayerShooter.cs

---
+++ Assets/Scripts/PlayerShooter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace schmup {
+    public class PlayerShooter : MonoBehaviour
+    {
+        [SerializeField] GameObject bulletPrefab;
+        [SerializeField] Transform firePoint;
+        [SerializeField] float fireRate = 0.2f;
+
+        InputReader input;
+        float nextFireTime;
+
+        void Start() {
+            input = GetComponent<InputReader>();
+        }
+
+        void Update() {
+            if (input.Shoot && Time.time >= nextFireTime) {
+                nextFireTime = Time.time + fireRate;
+                Vector3 spawnPos = new Vector3(firePoint.position.x, firePoint.position.y, -5f);
+                Instantiate(bulletPrefab, spawnPos, Quaternion.identity);
+            }
+        }
+    }
+}
