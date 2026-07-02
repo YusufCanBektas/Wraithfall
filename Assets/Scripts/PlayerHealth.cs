ef05586c492995e47334c446b74996f60f47fcda
YusufCanBektas <yusufcan.bektas@hotmail.com>
2026-07-02 13:15:56 +0200

    create PlayerHealth.cs

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace schmup {
+    public class PlayerHealth : MonoBehaviour
+    {
+        [SerializeField] int maxHealth = 3;
+        int currentHealth;
+
+        void Start()
+        {
+            currentHealth = maxHealth;
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            // EnemyBullet trifft Spieler
+            if (other.CompareTag("EnemyBullet"))
+            {
+                TakeDamage(1);
+                Destroy(other.gameObject);
+            }
+
+            // Gegner kollidiert mit Spieler
+            if (other.CompareTag("Enemy"))
+            {
+                TakeDamage(1);
+            }
+        }
+
+        void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            Debug.Log($"Leben: {currentHealth}");
+
+            if (currentHealth <= 0)
+            {
+                Debug.Log("Game Over!");
+                gameObject.SetActive(false); // Spieler verschwindet
+            }
+        }
+
+        public int GetHealth() => currentHealth;
+        public int GetMaxHealth() => maxHealth;
+    }
+}
